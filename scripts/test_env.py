02c88cbdcc2670dd7b48b234d4024d49fa2903a3
Nate Lamarche <natealamarche@gmail.com>
2026-09-08 16:57:03 -0400

    formatted test_env.py

--- scripts/test_env.py
+++ scripts/test_env.py
@@ -16,32 +16,34 @@ def main():
         pendulum_damping=0.001,
         motor_torque_limit=0.5,
     )
-    
+
     dt = 0.01
     env = PendulumEnv(params=params, dt=dt, max_episode_steps=1000)
-    
+
     obs, info = env.reset()
 
     timestamps = 1000
-    
+
     state_history = np.empty((timestamps, 4))
     time_history = np.empty(timestamps)
 
     for i in range(timestamps):
-        
         action = env.action_space.sample()
         obs, reward, terminated, truncated, info = env.step(action)
-        
+
         assert np.all(np.isfinite(obs))
-        
+
         state_history[i, :] = env.simulator.get_state()
         time_history[i] = i * dt
-        
+
         if terminated or truncated:
-            print(f"{"Terminated" if terminated else "Truncated"} at i={i} and steps={env.steps}")
+            print(
+                f"{'Terminated' if terminated else 'Truncated'} at i={i}"
+            )
             obs, info = env.reset()
-    
+
     plot_state_history(time_history, state_history)
-    
+
+
 if __name__ == "__main__":
     main()
